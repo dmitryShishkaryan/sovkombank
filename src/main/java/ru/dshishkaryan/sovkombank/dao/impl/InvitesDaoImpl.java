768521e64980ccc73c7da0036a20845aa87e69c4
Dmitry Shishkin <shishkindima@gmail.com>
2020-11-22 20:23:58 +0300

    fix

--- src/main/java/ru/dshishkaryan/sovkombank/dao/impl/InvitesDaoImpl.java
+++ src/main/java/ru/dshishkaryan/sovkombank/dao/impl/InvitesDaoImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Repository
 @AllArgsConstructor
-public class InvitesDaoDaoImpl implements InvitesDao {
+public class InvitesDaoImpl implements InvitesDao {
 
     private final DataSource dataSource;
 
